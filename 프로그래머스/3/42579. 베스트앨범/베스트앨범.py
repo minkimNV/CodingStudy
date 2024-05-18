2731cfb57eff39a52553ca528d0890141a3a355f
Minhee Kim <150657776+minkimNV@users.noreply.github.com>
2024-05-18 14:04:34 +0900

    [level 3] Title: 베스트앨범, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.py
@@ -1,61 +1,64 @@
-from collections import defaultdict
+# def solution(genres, plays):
 
-def solution(genres, plays):
-    zipping = [[genres[i], plays[i], i] for i in range(len(genres))]
-    hashbrown = defaultdict(list)  # 리스트를 값으로 가지는 defaultdict 생성
+#     songs = [[genre, (play, i)] for genre, (play, i) in zip(genres, enumerate(plays))]
+#     songs = sorted(songs, key = lambda x: (x[0], -x[1][1], x[1][0]))
 
-    # 딕셔너리 = {장르: [(재생횟수, 고유번호), (재생횟수, 고유번호), ...]}
-    for x in zipping:
-        genre, play, id = x[0], x[1], x[2]
-        hashbrown[genre].append((play, id))
 
-    # 총 재생 횟수 기준 장르 정렬
-    sorted_genres = sorted(hashbrown.keys(),
-                           key=lambda genre: sum(play for play, _ in hashbrown[genre]), reverse=True)
-    
-    # 장르 내에서 상위 2곡의 인덱스 추출
-    answer = []
-    for genre in sorted_genres:
-        top2 = [id for _, id in sorted(hashbrown[genre], key=lambda x: (-x[0], x[1]))[:2]]
-        answer.extend(top2)
+#     # 재생횟수 많은 장르 찾기
+#     gorder = {}
+#     for i in range(len(songs)):
+#         if songs[i][0] not in gorder:
+#             gorder[songs[i][0]] = songs[i][1][1]
+#         else:
+#             gorder[songs[i][0]] += songs[i][1][1]
 
-    return answer
+#     sortedgorder = sorted(gorder.items(), key=lambda x: -x[1])
 
-# 예시 테스트
+#     # 상위 2 곡 인덱스
+#     answer = []
+#     cnt = 0
+#     for genre in sortedgorder:
+#         cnt = 0
+#         for song in songs:
+#             if genre[0] == song[0]:
+#                 cnt += 1
+#                 if cnt > 2 :
+#                     break
+#                 else:
+#                     answer.append(song[1][0])
+
+#     return answer
+
 # print(solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500]))
 
-
-
 def solution(genres, plays):
-
-    songs = [[genre, (play, i)] for genre, (play, i) in zip(genres, enumerate(plays))]
-    songs = sorted(songs, key = lambda x: (x[0], -x[1][1], x[1][0]))
-
-
-    # 재생횟수 많은 장르 찾기
-    gorder = {}
-    for i in range(len(songs)):
-        if songs[i][0] not in gorder:
-            gorder[songs[i][0]] = songs[i][1][1]
-        else:
-            gorder[songs[i][0]] += songs[i][1][1]
-
-    sortedgorder = sorted(gorder.items(), key=lambda x: -x[1])
-
-    # 상위 2 곡 인덱스
     answer = []
-    cnt = 0
-    for genre in sortedgorder:
+
+    songs = []
+    for genre, (play, i) in zip(genres, enumerate(plays)):
+        songs.append([genre, (play, i)])
+    print(songs)
+
+    genre_sort = sorted(songs, key = lambda x: (x[0], -x[1][1], x[1][0]))
+    print(genre_sort)
+
+    total_play = {}
+    for song in genre_sort:
+        if song[0] not in total_play:
+            total_play[song[0]] = song[1][1]
+        else:
+            total_play[song[0]] += song[1][1]
+
+    total_play = dict(sorted(total_play.items(), key = lambda x: -x[1]))
+    print(total_play)
+
+    for genre, _ in total_play.items():
         cnt = 0
-        for song in songs:
-            if genre[0] == song[0]:
+        for g, (p, i) in genre_sort:
+            if g == genre:
                 cnt += 1
-                if cnt > 2 :
+                answer.append(p)
+                if cnt == 2:
                     break
-                else:
-                    answer.append(song[1][0])
 
     return answer
-
-print(solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500]))
-
